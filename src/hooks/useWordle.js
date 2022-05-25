b36a5a336320db9963578899e171dddb71800045
elijahsamuels <elijahsamuels@gmail.com>
2022-05-25 16:29:31 -0600

    added conditional checks for word based on length, repeated word, and number of guesses.

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,30 +4,50 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState("");
   const [guesses, setGuesses] = useState([]);
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState(["hello", "brace"]);
   const [isCorrect, setIsCorrect] = useState(false);
 
-  const formatGuess = () => {};
+  const formatGuess = () => {
+		console.log("formatting guess", currentGuess);
+		return
+
+	};
 
   const addNewGuess = () => {};
 
   const handleKeyup = ({ key }) => {
+    if (key === "Enter") {
+      // submit word
+      if (turn > 5) {
+				console.log("no more guesses available");
+				return
+      }
+      if (history.includes(currentGuess)) {
+				console.log("already tried this word", currentGuess);
+				return
 
-		if (key === 'Backspace'){
-			setCurrentGuess((prev) => {
-				return prev.slice(0, -1)
-			})
-			return
-		}
-		if(/^[a-zA-Z]$/.test(key)){
-			if(currentGuess.length < 5) {
-				setCurrentGuess((prev) => {
-					return prev + key
-				})
-			}
+      }
+      if (currentGuess.length !== 5) {
+				console.log("word is not long enough");
+				return
+      }
+			formatGuess()
+    }
 
-		}
-	};
+    if (key === "Backspace") {
+      setCurrentGuess((prev) => {
+        return prev.slice(0, -1);
+      });
+      return;
+    }
+    if (/^[a-zA-Z]$/.test(key)) {
+      if (currentGuess.length < 5) {
+        setCurrentGuess((prev) => {
+          return prev + key;
+        });
+      }
+    }
+  };
 
   return { turn, currentGuess, guesses, isCorrect, handleKeyup };
 };
