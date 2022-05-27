81adc357025973b439e9aa1a26d9c4bc32843347
elijahsamuels <elijahsamuels@gmail.com>
2022-05-26 22:49:43 -0600

    added logic in useEffect to check if the game has been won or lost.

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,31 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
+import Modal from "./Modal";
 
 function Wordle({ solution }) {
-  const { currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys } = useWordle(solution);
+  const {
+    currentGuess,
+    handleKeyup,
+    guesses,
+    isCorrect,
+    turn,
+    usedKeys,
+  } = useWordle(solution);
+  const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyup);
 
+    // if the player wins
+    if (isCorrect) {
+      setTimeout(() => setShowModal(true), 2000);
+      window.removeEventListener("keyup", handleKeyup);
+    }
+
+    // if the player loses
+    if (turn > 5) {
+      setTimeout(() => setShowModal(true), 2000);
+      window.removeEventListener("keyup", handleKeyup);
+    }
+
     return () => {
       window.removeEventListener("keyup", handleKeyup);
     };
-  }, [handleKeyup]);
-
-  // useEffect(() => {
-  // 	console.log("guesses:", guesses)
-  // 	console.log("isCorrect:", isCorrect)
-  // 	console.log("turn:", turn)
-  // }, [guesses, isCorrect, turn])
+  }, [handleKeyup, isCorrect, turn]);
 
   return (
     <div>
       <p>solution: {solution}</p>
       <p>currentGuess: {currentGuess}</p>
-			<Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
-			<Keypad usedKeys={usedKeys} />
+      <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+      <Keypad usedKeys={usedKeys} />
+      {showModal && (
+        <Modal isCorrect={isCorrect} turn={turn} solution={solution} />
+      )}
     </div>
   );
 }
