4fdebbfee9ed2747361eff5e54153a5380e5c0f4
elijahsamuels <elijahsamuels@gmail.com>
2022-05-26 22:23:04 -0600

    added logic for usedKeys

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -6,6 +6,7 @@ const useWordle = (solution) => {
   const [guesses, setGuesses] = useState([...Array(6)]);
   const [history, setHistory] = useState([]);
   const [isCorrect, setIsCorrect] = useState(false);
+  const [usedKeys, setUsedKeys] = useState({});
 
   const formatGuess = () => {
     let solutionArray = [...solution];
@@ -32,21 +33,49 @@ const useWordle = (solution) => {
   };
 
   const addNewGuess = (formattedGuess) => {
+
     if (currentGuess === solution) {
       setIsCorrect(true);
     }
+
     setGuesses((prevGuesses) => {
       let newGuesses = [...prevGuesses];
       newGuesses[turn] = formattedGuess;
       return newGuesses;
     });
+
     setHistory((prevHistory) => {
       return [...prevHistory, currentGuess];
     });
-		setTurn((prevTurn) => {
-			return prevTurn + 1
+
+    setTurn((prevTurn) => {
+      return prevTurn + 1;
+    });
+
+		setUsedKeys((prevUsedKeys) => {
+			let newKeys = {...prevUsedKeys}
+
+			formattedGuess.forEach((letter) => {
+				const currentColor = newKeys[letter.key]
+
+				if (letter.color === 'green'){
+					newKeys[letter.key] = 'green'
+					return
+				}
+				if (letter.color === 'yellow' && currentColor !== 'green'){
+					newKeys[letter.key] = 'yellow'
+					return
+				}
+				if (letter.color === 'grey' && currentColor !== 'green' && currentColor !== 'yellow'){
+					newKeys[letter.key] = 'grey'
+					return
+				}
+			})
+			console.log(newKeys)
+			return newKeys
 		})
-		setCurrentGuess('')
+
+    setCurrentGuess("");
   };
 
   const handleKeyup = ({ key }) => {
@@ -86,7 +115,7 @@ const useWordle = (solution) => {
     }
   };
 
-  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+  return { turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup };
 };
 
 export default useWordle;
