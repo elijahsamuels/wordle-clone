f4c110fef1fb4d2cf12dc830896496c66524861f
elijahsamuels <elijahsamuels@gmail.com>
2022-05-26 19:25:31 -0600

    added logic for Row component to build out grid

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React from "react";
 
-function Row() {
-	return (
-		<div className="row">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-		</div>
-	)
+function Row({ guess, currentGuess }) {
+  if (guess !== undefined) {
+    return (
+      <div className="row past">
+        {guess.map((letter, index) => {
+          return (
+            <div key={index} className={letter.color}>
+              {letter.key}
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+
+  if (currentGuess) {
+    let letters = currentGuess.split("");
+
+    return (
+      <div className="row current">
+        {letters.map((letter, index) => (
+          <div key={index} className="filled">
+            {letter}
+          </div>
+        ))}
+        {[...Array(5 - letters.length)].map((value, index) => {
+          return <div key={index}></div>;
+        })}
+      </div>
+    );
+  }
+
+  return (
+    <div className="row">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
