76eaeaf1ed35264bde5ffcdea75665270be07b11
elijahsamuels <elijahsamuels@gmail.com>
2022-05-26 22:23:56 -0600

    imported usedKeys

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,11 +1,10 @@
 import React, { useEffect } from "react";
 import useWordle from "../hooks/useWordle";
 import Grid from "./Grid";
+import Keypad from "./Keypad";
 
 function Wordle({ solution }) {
-  const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(
-    solution
-  );
+  const { currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys } = useWordle(solution);
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyup);
@@ -26,6 +25,7 @@ function Wordle({ solution }) {
       <p>solution: {solution}</p>
       <p>currentGuess: {currentGuess}</p>
 			<Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
+			<Keypad usedKeys={usedKeys} />
     </div>
   );
 }
